6b18a2d92be1ae50c17130290117509bb385f34f
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2024-07-03 07:33:18 +0530

    star rating machine coding added

---
+++ Star_Rating/src/components/StarRating.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+const StarRating = ({starCount = 5}) => {
+    const [starValue, setStarValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState(0);
+  return (
+    <div className="container">
+        {new Array(starCount).fill(0).map((value, index) => {
+            return (
+            <span key={index}
+              className={hoverValue === 0 && index < starValue || index < hoverValue ? "gold" : ""}
+              onClick={() => setStarValue(index+1)}
+              onMouseEnter={() => setHoverValue(index + 1)}
+              onMouseLeave={() => setHoverValue(0)}
+            >
+               &#9733;
+            </span>
+            )
+        })}
+    </div>
+  )
+}
+
+export default StarRating
