1bb3fd247854e429be40cfe71df81ff053ff7224
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2024-07-03 07:45:50 +0530

    readable added

--- Star_Rating/src/components/StarRating.tsx
+++ Star_Rating/src/components/StarRating.tsx
@@ -8,7 +8,7 @@ const StarRating = ({starCount = 5}) => {
         {new Array(starCount).fill(0).map((value, index) => {
             return (
             <span key={index}
-              className={hoverValue === 0 && index < starValue || index < hoverValue ? "gold" : ""}
+            className={(hoverValue === 0 && index < starValue) || (index < hoverValue) ? "gold" : ""}
               onClick={() => setStarValue(index+1)}
               onMouseEnter={() => setHoverValue(index + 1)}
               onMouseLeave={() => setHoverValue(0)}
